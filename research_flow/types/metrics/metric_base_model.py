6a6e7272432c4462cc3901c3f67b574b813e1db3
castelojb <joaocb14@gmail.com>
2024-11-08 18:36:02 -0300

    chore: add score type in class definition

--- research_flow/types/metrics/metric_base_model.py
+++ research_flow/types/metrics/metric_base_model.py
@@ -1,15 +1,14 @@
 from abc import ABC, abstractmethod
 from typing import Generic
 
-from gloe import AsyncTransformer
+from gloe import Transformer
 from pydantic import BaseModel, ConfigDict
 
 from research_flow.machine_learning.base_machine_learning_algorithm import DataType
-from research_flow.types.comon_types import PatientIdx
-from research_flow.types.metrics.metric_score_model import MetricScoreModel
+from research_flow.types.comon_types import Score
 
 
-class MetricBaseModel(BaseModel, Generic[DataType], ABC):
+class MetricBaseModel(BaseModel, Generic[DataType, Score], ABC):
     model_config = ConfigDict(arbitrary_types_allowed=True)
 
     # metric_name: str
@@ -22,5 +21,5 @@ class MetricBaseModel(BaseModel, Generic[DataType], ABC):
     @abstractmethod
     def get_metric(
         self,
-    ) -> AsyncTransformer[tuple[PatientIdx, DataType], list[MetricScoreModel]]:
+    ) -> Transformer[DataType, Score]:
         pass
