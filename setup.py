2cd4c0767fde67f153789eb8f090f015f59ad767
José Souza <joseedsouza.dev@gmail.com>
2025-05-01 22:31:38 -0300

    fix: update license classifier to Apache Software License

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.10",
